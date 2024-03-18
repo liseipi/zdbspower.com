f6a9346cedcb9a89452e51a0681341686a0b8f8b
huiliu <hui@qq.com>
2024-03-18 09:41:56 +0800

    改名字

--- docs/_nuxt/static/1676017233/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact","\u002Fnews","\u002Fabout","\u002Fnet","\u002Fproducts","\u002Fnews\u002Fshow\u002F28","\u002Fnews\u002Fshow\u002F26","\u002Fnews\u002Fshow\u002F25","\u002Fnews\u002Fshow\u002F27","\u002Fnews\u002Fshow\u002F24","\u002Fnews\u002Fshow\u002F23","\u002Fnews\u002Fshow\u002F22"]})
---
+++ docs/_nuxt/static/1710726027/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact","\u002Fabout","\u002Fnet","\u002Fnews","\u002Fproducts","\u002Fnews\u002Fshow\u002F25","\u002Fnews\u002Fshow\u002F28","\u002Fnews\u002Fshow\u002F27","\u002Fnews\u002Fshow\u002F24","\u002Fnews\u002Fshow\u002F23","\u002Fnews\u002Fshow\u002F22","\u002Fnews\u002Fshow\u002F26"]})
